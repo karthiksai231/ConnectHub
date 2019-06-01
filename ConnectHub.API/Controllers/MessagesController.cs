819dee863b763f3d2088590b1e05963bb8402e19
karthik pamidimukkala <karthiksai231@gmail.com>
2019-06-01 17:50:35 -0400

    Adding messaging features

--- ConnectHub.API/Controllers/MessagesController.cs
+++ ConnectHub.API/Controllers/MessagesController.cs
@@ -79,7 +79,9 @@ namespace ConnectHub.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
                 return Unauthorized();
             }
 
@@ -96,14 +98,44 @@ namespace ConnectHub.API.Controllers
 
             _repo.Add(message);
 
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
-
             if(await _repo.SaveAll()) 
             {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
+
                 return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
             }
 
             throw new Exception("Error creating message");
         }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id, int userId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+                return Unauthorized();
+            }
+
+            var message = await _repo.GetMessage(id);
+
+            if (message.SenderId == userId) {
+                message.SenderDeleted = true;
+            }
+
+            if (message.RecipientId == userId) {
+                message.RecipientDeleted = true;
+            }
+
+            if (message.SenderDeleted && message.RecipientDeleted) {
+                _repo.Delete(message);
+            }
+
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+
+            throw new Exception("Unable to delete the message");
+        }
+
     }
 }
